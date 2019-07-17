e938d45ddc182bc1a455c61c94855189cd91733e
foggybrains <dustin.keyes@gmail.com>
2019-07-17 14:12:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+
+function writeCards(nameArray, event) {
+    for (let i = 0; i < nameArray.length; i++) {
+        nameArray[i] = `Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`
+    }
+    return nameArray
+}
+
+function countdown(i) {
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
